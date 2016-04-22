11ff230243f0207b4878235c6f713fcde1a39587
Jon Nordby <jononor@gmail.com>
2016-04-22 18:31:55 +0200

    Improve MicroFlo warning message a bit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.transports = {
 try {
   exports.transports.microflo = require('./src/microflo');
 } catch (e) {
-  console.log('MicroFlo transport unavailable: ' + e.message);
+  console.log('fbp-protocol-client: microflo:// transport unavailable: ' + e.message);
 }
 
 exports.getTransport = function (transport) {
